b04b37567ced5dd2a3e6a7be94164f0b4e7786ac
HassanTofayli <hassantofayli2@gmail.com>
2023-09-08 16:00:18 +0300

    filtering products by category

---
+++ Data/Components/CategoriesViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingCart.Data.Components
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+        public CategoriesViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() => View(await _context.Categories.ToListAsync());
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:DbConnection"]);
+});
+
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.IsEssential = true;
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -8,16 +25,24 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
+app.MapControllerRoute(
+    name: "products",
+    pattern: "/products/{categorySlug?}",
+    defaults: new { controller = "Products", action = "Index" });
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<AppDbContext>();
+SeedData.SeedDatabase(context);
 
 app.Run();
