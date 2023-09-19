d459504d2dfddc8601ba0b95aa7fc96c9f5a4077
HassanTofayli <hassantofayli2@gmail.com>
2023-09-19 06:29:26 +0300

    Adding Identity

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,70 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Models;
+using ShoppingCart.Models.ViewModels;
 
 namespace ShoppingCart.Controllers
 {
 	public class AccountController : Controller
 	{
-		public IActionResult Index()
+		private UserManager<AppUser> _userManager;
+		private SignInManager<AppUser> _signInManager;
+		public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
 		{
-			return View();
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+
+		public IActionResult Create() => View();
+
+		[HttpPost]
+		public async Task<IActionResult> Create(User user)
+		{
+			if (ModelState.IsValid)
+			{
+				AppUser newUser = new AppUser { UserName = user.UserName, Email = user.Email };
+				IdentityResult result = await _userManager.CreateAsync(newUser, user.Password);
+
+				if (result.Succeeded)
+				{
+					return Redirect("/admin/products");
+				}
+
+				foreach (IdentityError error in result.Errors)
+				{
+					ModelState.AddModelError("", error.Description);
+				}
+
+			}
+
+			return View(user);
+		}
+
+		public IActionResult Login(string returnUrl) => View(new LoginViewModel { ReturnUrl = returnUrl });
+
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel loginVM)
+		{
+			if (ModelState.IsValid)
+			{
+				Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(loginVM.UserName, loginVM.Password, false, false);
+
+				if (result.Succeeded)
+				{
+					return Redirect(loginVM.ReturnUrl ?? "/");
+				}
+
+				ModelState.AddModelError("", "Invalid username or password");
+			}
+
+			return View(loginVM);
+		}
+
+		public async Task<RedirectResult> Logout(string returnUrl = "/")
+		{
+			await _signInManager.SignOutAsync();
+
+			return Redirect(returnUrl);
 		}
 	}
 }
